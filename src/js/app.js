13f74a0fb3cd7183e14e208e5d1b1433561333f7
Alexey Sokolov <dabero7111@gmail.com>
2024-04-09 22:40:10 +0300

    advanced

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+export default function orderByProps(obj, arr) {
+	const tempArr = [];
+	arr.forEach((key) => {
+	  if (obj.hasOwnProperty(key)) {
+		 tempArr.push({ key, value: obj[key] });
+		 delete obj[key];
+	  }
+	});
+	const tempObj = [];
+	for (const key in obj) {
+	  if (obj.hasOwnProperty(key)) {
+		 tempObj.push({ key, value: obj[key] });
+	  }
+	}
+	tempObj.sort((a, b) => a.key.localeCompare(b.key));
+ 
+	const result = [...tempArr, ...tempObj];
+ 
+	return result;
+ }
